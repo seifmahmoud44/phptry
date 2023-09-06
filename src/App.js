b5adbdb97f3324de71e97d57cd8057c3eaca5f29
seif <seenaseif@gmail.com>
2023-09-06 23:28:32 +0300

    try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 function App() {
+  const [name, setName] = useState();
+
+  const getDate = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/articles", article)
+      .then((response) => setArticleId(response.data.id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getDate}>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
